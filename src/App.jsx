33127bf93a44214951ccfa7c580db94e65020762
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-14 20:06:50 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 font-sans antialiased">
+      <header className="bg-white shadow-md p-6">
+        <div className="container mx-auto">
+          <h1 className="text-3xl font-bold text-center text-gary-800">Productos</h1>
+        </div>
+      </header>
+
+      <main className="container mx-auto py-8">
+
+      </main>
+    </div>
+  );
+}
+
+export default App
